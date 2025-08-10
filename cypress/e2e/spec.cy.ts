f37fe3dd3239082f1cebaf732f34bcd1c618a129
Anish Batra <anishbatra@MacBookPro.lan>
2025-08-10 14:07:54 -0400

    updated DD code

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -14,4 +14,82 @@ describe("My First Test", () => {
     //  Verify that the value has been updated
     cy.get(".action-email", { timeout: 10000 }).should("have.value", "fake@email.com");
   });
+
+  it("Visits the Demo Site", () => {
+  // Example Allure labels (also visible in rich test reporting)
+  // @ts-ignore
+  cy.allure().label('team','qa').label('component','frontend').severity('normal');
+    cy.visit("https://example.cypress.io", { timeout: 10008 });
+    cy.contains("type").click({ timeout: 10000 });
+    // Should be on a new URL which
+    // includes '/commands/actions'
+    cy.url().should("include", "/commands/actions");
+    // Get an input, type into it
+    cy.get(".action-email", { timeout: 10000 }).type("fake@email.com");
+
+    //  Verify that the value has been updated
+    cy.get(".action-email", { timeout: 10000 }).should("have.value", "fake@email.com");
+  });
+
+  it("Visits the Demo Site", () => {
+  // Example Allure labels (also visible in rich test reporting)
+  // @ts-ignore
+  cy.allure().label('team','qa').label('component','frontend').severity('normal');
+    cy.visit("https://example.cypress.io", { timeout: 10008 });
+    cy.contains("type").click({ timeout: 10000 });
+    // Should be on a new URL which
+    // includes '/commands/actions'
+    cy.url().should("include", "/commands/actions");
+    // Get an input, type into it
+    cy.get(".action-email", { timeout: 10000 }).type("fake@email.com");
+
+    //  Verify that the value has been updated
+    cy.get(".action-email", { timeout: 10000 }).should("have.value", "fake@email.com");
+  });
+
+  it("Visits the Demo Site", () => {
+  // Example Allure labels (also visible in rich test reporting)
+  // @ts-ignore
+  cy.allure().label('team','qa').label('component','frontend').severity('normal');
+    cy.visit("https://example.cypress.io", { timeout: 10008 });
+    cy.contains("type").click({ timeout: 10000 });
+    // Should be on a new URL which
+    // includes '/commands/actions'
+    cy.url().should("include", "/commands/actions");
+    // Get an input, type into it
+    cy.get(".action-email", { timeout: 10000 }).type("fake@email.com");
+
+    //  Verify that the value has been updated
+    cy.get(".action-email", { timeout: 10000 }).should("have.value", "fake@email.com");
+  });
+  it("Visits the Demo Site", () => {
+  // Example Allure labels (also visible in rich test reporting)
+  // @ts-ignore
+  cy.allure().label('team','qa').label('component','frontend').severity('normal');
+    cy.visit("https://example.cypress.io", { timeout: 10008 });
+    cy.contains("type").click({ timeout: 10000 });
+    // Should be on a new URL which
+    // includes '/commands/actions'
+    cy.url().should("include", "/commands/actions");
+    // Get an input, type into it
+    cy.get(".action-email", { timeout: 10000 }).type("fake@email.com");
+
+    //  Verify that the value has been updated
+    cy.get(".action-email", { timeout: 10000 }).should("have.value", "fake12@email.com");
+  });
+  it("Visits the Demo Site", () => {
+  // Example Allure labels (also visible in rich test reporting)
+  // @ts-ignore
+  cy.allure().label('team','qa').label('component','frontend').severity('normal');
+    cy.visit("https://example.cypress.io", { timeout: 10008 });
+    cy.contains("type").click({ timeout: 10000 });
+    // Should be on a new URL which
+    // includes '/commands/actions'
+    cy.url().should("include", "/commands/actions");
+    // Get an input, type into it
+    cy.get(".action-email", { timeout: 10000 }).type("fake@email.com");
+
+    //  Verify that the value has been updated
+    cy.get(".action-email", { timeout: 10000 }).should("have.value", "fake123@email.com");
+  });
 });
