01dcd3aa4b99139e8242d3d4027f01c512c47007
Anish Batra <anishbatra@MacBookPro.lan>
2025-08-10 13:41:17 -0400

    added datadog for visibility

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,5 @@
 describe("My First Test", () => {
-  it("Visits the Kitchen Sink", () => {
+  it("Visits the Demo Site", () => {
   // Example Allure labels (also visible in rich test reporting)
   // @ts-ignore
   cy.allure().label('team','qa').label('component','frontend').severity('normal');
